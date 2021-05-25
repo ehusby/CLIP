a641e98c889b3c9b7c07cd88265cbd21b2f965eb
husby036 <husby036@umn.edu>
2021-05-24 21:16:04 -0500

    Code refactoring

--- ITEMS__LINE__to__CSSTRINGS.py
+++ ITEMS__LINE__to__CSSTRINGS.py
@@ -2,4 +2,4 @@ from lib import base_func
 base_func.test_pyperclip_import_standalone()
 import pyperclip
 
-pyperclip.copy("'"+"', '".join(pyperclip.paste().splitlines())+"'")
+pyperclip.copy("'{}'".format("', '".join(pyperclip.paste().splitlines())))
