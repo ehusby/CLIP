c1c7d2d95b6ecdf0ea0bc7a3683c0f2a26e87ac6
Erik Husby <20846681+ehusby@users.noreply.github.com>
2024-04-01 15:10:56 +0800

    Handle quoted source path

--- PATH__WINDOWS__to__WSL.py
+++ PATH__WINDOWS__to__WSL.py
@@ -6,7 +6,7 @@ import pyperclip
 try:
     import re
 
-    pyperclip.copy(re.sub(r"^\s*([A-Z]):", lambda m: f"/mnt/{m.group(1).lower()}", re.sub(r"\\+", "/", pyperclip.paste())))
+    pyperclip.copy(re.sub(r"^([\"\']*)\s*([A-Z]):", lambda m: f"{m.group(1)}/mnt/{m.group(2).lower()}", re.sub(r"\\+", "/", pyperclip.paste())))
 
 except:
     base_func.display_error_message()
